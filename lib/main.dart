fad6ab5ed9bf36c55789f17398e6d2fe0bc9cd7f
Seif Tarek <SeifTarek@gmail.com>
2026-03-25 00:51:01 +0200

    start

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:snap_shot/snap_shot.dart';
+
+void main(List<String> args) {
+  runApp(const SnapShot());
+}
---
+++ lib/snap_shot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SnapShot extends StatelessWidget {
+  const SnapShot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    debugShowCheckedModeBanner: false,
+      );
+  }
+}
