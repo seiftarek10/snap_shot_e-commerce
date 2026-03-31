54cdc1bd87c87777380c051dd52c194c8e6e81d5
Seif Tarek <SeifTarek@gmail.com>
2026-03-31 20:38:31 +0200

    finish all categories list

--- lib/features/Home/presentation/view/screens/home_view.dart
+++ lib/features/Home/presentation/view/screens/home_view.dart
@@ -3,6 +3,7 @@ import 'package:snap_shot/core/constants/space.dart';
 import 'package:snap_shot/core/widgets/page_padding.dart';
 import 'package:snap_shot/core/widgets/search_field.dart';
 import 'package:snap_shot/core/widgets/snap_shop_word.dart';
+import 'package:snap_shot/features/Home/presentation/view/widgets/all_categories_list.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -12,10 +13,14 @@ class HomeView extends StatelessWidget {
     return SingleChildScrollView(
       child: PagePadding(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             AppSpace.instance.topPageSpace,
             SnapShotWord(),
+            AppSpace.instance.v16,
             SearchField(),
+            AppSpace.instance.v16,
+            AllCategoriesList(),
           ],
         ),
       ),
